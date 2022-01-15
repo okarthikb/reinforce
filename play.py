bfdacfe9623c560137aa34b09e8b004a0f044e5c
okarthikb <86470305+okarthikb@users.noreply.github.com>
2022-01-15 12:55:12 +0530

    Update play.py

--- play.py
+++ play.py
@@ -2,13 +2,15 @@ from net import *
 
 
 try:
-  loc = prefix + "320.pt"
+  epoch = int(sys.argv[1])
+  loc = prefix + f"{epoch}.pt"
   print("using model @", loc)
   state_dict = torch.load(loc)
   pi.load_state_dict(state_dict)
 except:
-  print("using default instead")
+  print("using default instead'")
 
 
-print(f"return: {sum(tau(render=True)[2])}")
-# print(f"avg return of 100 eps: {np.mean([sum(tau()[2]) for _ in range(100)])}")
+R = tau(True)[2]
+print(f"return: {sum(R)}")
+# print(f"avg return of 10 eps: {np.mean([sum(tau()[2]) for _ in range(10)])}")

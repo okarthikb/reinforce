2e35b2602065070d19d1e331a1089157ba93cd3d
okarthikb <86470305+okarthikb@users.noreply.github.com>
2022-01-15 12:54:40 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,18 +3,22 @@ from net import *
 
 # adjust hyperparameters
 lr = 2.5e-4
-epochs = 500
+epochs = 600
 N = 20  # E[R] ~ sum(pi(a_t | s_t)V_t) / N
-mod = 10  # checkpoint once every mod epochs
+mod = 5  # checkpoint once every mod epochs
 gamma = 0.99  # reward discount
 
+# init optimiser
 opt = torch.optim.Adam(pi.parameters(), lr=lr)
 
-lossesf = open(prefix + "losses.txt", "w")
-rewardsf = open(prefix + "rewards.txt", "w")
+# create files to log epoch losses and rewards
+lossesf = open(prefix + f"losses.txt", "w")
+rewardsf = open(prefix + f"rewards.txt", "w")
 
+# train network
 losses, rewards = train(epochs, N, mod, gamma, opt)
 
+# plot stuff
 fig = plt.figure(figsize=(8, 8))
 
 x = list(range(1, epochs + 1))
@@ -22,11 +26,11 @@ x = list(range(1, epochs + 1))
 plt.xlabel("epoch")
 plt.ylabel("loss")
 plt.plot(x, losses)
-plt.savefig(prefix + "losses.png")
+plt.savefig(prefix + f"losses.png")
 
 plt.clf()
 
 plt.xlabel("epoch")
 plt.ylabel("reward")
 plt.plot(x, rewards)
-plt.savefig(prefix + "rewards.png")
+plt.savefig(prefix + f"rewards.png")
